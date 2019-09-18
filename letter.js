b1c4683f6986debf3ce8f79f6e706d3bdcd4d0b2
Guillermo Villalta <mexcelus@gmail.com>
2019-09-18 15:22:18 -0600

    new constructor Letter

---
+++ letter.js
@@ -0,0 +1,10 @@
+function Letter(string) {
+    this.wordPlaceholder = [];
+    this.guessed = false;
+    this.guessedLetter = function () {
+        console.log(string);
+    };
+    this.letterChecker =  function () {
+        
+    }
+}
