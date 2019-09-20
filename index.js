8d22a55244292aef7209971929da0e9f8c070ac0
Guillermo Villalta <mexcelus@gmail.com>
2019-09-20 12:39:16 -0600

    created variables

---
+++ index.js
@@ -0,0 +1,11 @@
+var Word = require('./word.js');
+
+remainingGuesses = 10;
+arrayOfOptions = [
+    'Abstraction',
+    'Binary',
+    'Callback Function',
+    'Search Engine',
+    'Server'
+];
+
