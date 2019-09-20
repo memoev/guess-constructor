4c424a86880e57e3aef4f29e269122728706c819
Guillermo Villalta <mexcelus@gmail.com>
2019-09-20 10:42:48 -0600

    getting the ball rolling

--- letter.js
+++ letter.js
@@ -1,10 +1,16 @@
+example = 'This will be fun!'
+
 function Letter(string) {
     this.wordPlaceholder = [];
     this.guessed = false;
     this.guessedLetter = function () {
-        console.log(string);
+        let array =  string.split('');
+        console.log(array);
     };
-    this.letterChecker =  function () {
-        
+    this.letterChecker =  function () {      
     }
 }
+
+var newLetter = new Letter(example); // Never forget to actually create the instance from the constructor!
+
+newLetter.guessedLetter();
